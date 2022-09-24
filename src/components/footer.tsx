8e13a6d4528cfb7029daee14027381aa2ff44036
d-ivashchuk <dimaiv@me.com>
2022-09-24 12:00:28 +0200

    initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import NewsLetter from '../components/newsletter'
+import { FooterProps } from '../interfaces/Layout.interface'
+import { WebsiteFooter } from '@prisma/lens/dist/web'
+
+type FooterViewProps = {
+  footerProps: FooterProps
+}
+
+const FooterWrapper = styled.div`
+  background: transparent;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  color: ${(p) => p.theme.colors.gray500};
+  margin-top: 5rem;
+  > div {
+    width: 100%;
+  }
+  // > * {
+  //   .group {
+  //     a {
+  //       color: currentcolor !important;
+  //       text-decoration: none;
+
+  //       &:hover {
+  //         color: ${(p) => p.theme.colors.gray600} !important;
+  //       }
+  //     }
+  //   }
+  //   @media (min-width: 0px) and (max-width: ${(p) => p.theme.breakpoints.tablet}) {
+  //     flex-direction: column;
+  //     padding: ${(p) => p.theme.fontSizes[48]} 0.5rem;
+  //   }
+  // }
+`
+
+const FooterSec = ({ footerProps }: FooterViewProps) => {
+  const { newsletter } = footerProps
+  return (
+    <FooterWrapper>
+      <WebsiteFooter newsletterComponent={<NewsLetter newsletter={newsletter} />} />
+    </FooterWrapper>
+  )
+}
+
+export default FooterSec
---
+++ src/hooks/useAllArticlesQuery.ts
@@ -0,0 +1,34 @@
+import { graphql, useStaticQuery } from 'gatsby'
+import { AllArticles } from '../interfaces/AllArticles.interface'
+
+export const useAllArticlesQuery = () => {
+  const { allMdx }: AllArticles = useStaticQuery(graphql`
+    query {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              title
+              duration
+              staticLink
+              experimental
+              preview
+              earlyaccess
+              langSwitcher
+              search
+              dbSwitcher
+              hidePage
+              codeStyle
+            }
+            fields {
+              slug
+              modSlug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return { allMdx }
+}
