3d14991745f0e5301df951d86a9281c988273167
d-ivashchuk <dimaiv@me.com>
2022-09-25 15:16:31 +0200

    try reducing the number of images

--- plugins/gatsby-plugin-lost-pixel/gatsby-node.js
+++ plugins/gatsby-plugin-lost-pixel/gatsby-node.js
@@ -43,6 +43,7 @@ exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, plu
       }
     })
     .filter((edge) => edge !== null)
+    .slice(0, 100)
 
   await fsPromises.writeFile(outputFile, JSON.stringify(pages))
 }
