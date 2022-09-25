d812dae9c261ca2c2b35ab619318cbe43796e569
d-ivashchuk <dimaiv@me.com>
2022-09-25 22:57:02 +0200

    try odiff with more images

--- plugins/gatsby-plugin-lost-pixel/gatsby-node.js
+++ plugins/gatsby-plugin-lost-pixel/gatsby-node.js
@@ -43,7 +43,7 @@ exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, plu
       }
     })
     .filter((edge) => edge !== null)
-    .slice(0, 10)
+    .slice(0, 100)
 
   await fsPromises.writeFile(outputFile, JSON.stringify(pages))
 }
