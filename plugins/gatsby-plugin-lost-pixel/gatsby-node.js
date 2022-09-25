f41e2ad31d9220bfdc7b3038608ccc6f41f35d4d
d-ivashchuk <dimaiv@me.com>
2022-09-25 16:33:15 +0200

    try smaller chunk but still almost all imges

--- plugins/gatsby-plugin-lost-pixel/gatsby-node.js
+++ plugins/gatsby-plugin-lost-pixel/gatsby-node.js
@@ -43,6 +43,7 @@ exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, plu
       }
     })
     .filter((edge) => edge !== null)
+    .slice(0, 380)
 
   await fsPromises.writeFile(outputFile, JSON.stringify(pages))
 }
