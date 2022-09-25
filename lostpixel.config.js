67deb94c8ef7e7958cc0dc0182823d82df52d1e8
Chris Kalmar <christian.kalmar@gmail.com>
2022-09-25 23:56:58 +0200

    debug

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -3,16 +3,22 @@ module.exports = {
     // write your pages here
     pages: [],
     // you create your pages here & both are composed
-    pagesJsonUrl: 'http://172.17.0.1:9000/lost-pixel.json',
-    baseUrl: 'http://172.17.0.1:9000',
+    pagesJsonUrl: 'http://localhost:9000/lost-pixel.json',
+    baseUrl: 'http://localhost:9000',
   },
-  lostPixelProjectId: 'cl8fqnk7033927301m93b5rhis9',
-  ciBuildId: process.env.GITHUB_RUN_ID,
-  ciBuildNumber: process.env.GITHUB_RUN_NUMBER,
-  repository: process.env.REPOSITORY,
-  commitRef: process.env.GITHUB_REF,
-  commitRefName: process.env.GITHUB_REF_NAME,
-  commitHash: process.env.COMMIT_HASH,
+  // lostPixelProjectId: 'cl8fqnk7033927301m93b5rhis9',
+
+  imagePathBaseline: '/Users/chris/projects/tmp/lost-pixel-gatsby/.lostpixel/baseline/',
+  imagePathCurrent: '/Users/chris/projects/tmp/lost-pixel-gatsby/.lostpixel/current/',
+  imagePathDifference: '/Users/chris/projects/tmp/lost-pixel-gatsby/.lostpixel/difference/',
+
+  generateOnly: true,
+  // ciBuildId: process.env.GITHUB_RUN_ID,
+  // ciBuildNumber: process.env.GITHUB_RUN_NUMBER,
+  // repository: process.env.REPOSITORY,
+  // commitRef: process.env.GITHUB_REF,
+  // commitRefName: process.env.GITHUB_REF_NAME,
+  // commitHash: process.env.COMMIT_HASH,
   compareEngine: 'odiff',
   s3: {
     endPoint: 'ams3.digitaloceanspaces.com',
@@ -26,11 +32,11 @@ module.exports = {
       content: `iframe {
       visibility: hidden;
     }
-  
+
     section img {
       visibility: hidden;
     }
-    
+
     /* hide cookie banner */
     #onetrust-consent-sdk {
       display: none;
