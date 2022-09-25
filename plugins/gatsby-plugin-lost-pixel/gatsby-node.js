1104906f7c3cb9b9a4fcef3163e544112343918b
d-ivashchuk <dimaiv@me.com>
2022-09-25 15:55:50 +0200

    try another batch

--- plugins/gatsby-plugin-lost-pixel/gatsby-node.js
+++ plugins/gatsby-plugin-lost-pixel/gatsby-node.js
@@ -43,7 +43,7 @@ exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, plu
       }
     })
     .filter((edge) => edge !== null)
-    .slice(0, 250)
+    .slice(300)
 
   await fsPromises.writeFile(outputFile, JSON.stringify(pages))
 }
